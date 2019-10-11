53539ac461a9518b92119fe44537bbbdefb20b85
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-10-11 18:14:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,7 @@
 first_number = 2
 second_number = 3
 expect(second_number).not_to equal(0)
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
